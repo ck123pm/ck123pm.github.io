1f6bcd3885161aaaa9f59caa291fb2e7107fbf8b
jianshli <wxs-studio@cisco.com>
2020-05-07 23:44:59 +0800

    test

---
+++ ck123pm/deploy.sh
@@ -0,0 +1,13 @@
+check_result=`docker container ls -a|grep newblog`
+
+docker exec -it "$check_result" hexo d
+msg=$1
+if [ -n "$msg" ]; then
+   git add -A
+   git commit -m"${msg}"
+   git pull
+   git status
+   echo "完成add、commit、pull，别忘了push"
+else
+    echo "请添加注释再来一遍"
+fi
